c8bb9ea80541c8e99bd314c9721ac55de0c9c6a4
Hobby0113 <littlemoom@naver.com>
2019-12-16 11:19:58 +0000

    191216 docker-swarm 4.0

---
+++ hlf_tool.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo ln -s /home/jongseek98/fabric-samples/bin/cryptogen /usr/bin/cryptogen
+sudo ln -s /home/jongseek98/fabric-samples/bin/configtxgen /usr/bin/configtxgen
+sudo ln -s /home/jongseek98/fabric-samples/bin/configtxlator /usr/bin/configtxlator
+sudo ln -s /home/jongseek98/fabric-samples/bin/discover /usr/bin/discover
+sudo ln -s /home/jongseek98/fabric-samples/bin/fabric-ca-client /usr/bin/fabric-ca-client
+sudo ln -s /home/jongseek98/fabric-samples/bin/idemixgen /usr/bin/idemixgen
+sudo ln -s /home/jongseek98/fabric-samples/bin/orderer /usr/bin/orderer
